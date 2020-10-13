50cc33778af762ce8cfdc88c4883db25cd10a89b
Aidan Shannon <ajshannon@dmacc.edu>
2020-10-13 11:29:47 -0500

    test pass

--- fun_with_collections/basic_list.py
+++ fun_with_collections/basic_list.py
@@ -2,11 +2,15 @@
 Author: Aidan Shannon
 Program: basic_list.py
 
-
+takes your inputs and sticks them into a list
 """
 
 
 def make_list():
+    """
+    makes list for user's inputs
+    :return:
+    """
     u_list = []
     for i in range(0, 3):
         u_input = int(get_input())
@@ -15,4 +19,14 @@ def make_list():
 
 
 def get_input():
-    pass
+    """
+    gets inputs for the list
+    :return:
+    """
+    u_input = input("Give me a number: ")
+    return u_input
+
+
+if __name__ == '__main__':
+    test_list = make_list()
+    print(test_list)
