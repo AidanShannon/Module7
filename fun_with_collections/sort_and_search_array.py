7ddab4d25ce2591cec4c0aa4a97264a3f7285506
Aidan Shannon <ajshannon@dmacc.edu>
2020-10-13 21:44:24 -0500

    sort_array test pass

--- fun_with_collections/sort_and_search_array.py
+++ fun_with_collections/sort_and_search_array.py
@@ -23,7 +23,17 @@ def search_array():
 
 
 def sort_array():
-    pass
+    """
+    sorts array numerically
+    :return:
+    """
+    for i in range(0, len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            if numbers[i] > numbers[j]:
+                temp = numbers[i]
+                numbers[i] = numbers[j]
+                numbers[j] = temp
+    return numbers  # return works with test statement
 
 
 if __name__ == '__main__':
