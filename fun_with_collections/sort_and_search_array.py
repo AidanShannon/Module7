2bd6a8eb319321a6d06caf4fb72c073d116c9c43
Aidan Shannon <ajshannon@dmacc.edu>
2020-10-13 21:43:39 -0500

    sort_array test fail

--- fun_with_collections/sort_and_search_array.py
+++ fun_with_collections/sort_and_search_array.py
@@ -23,17 +23,7 @@ def search_array():
 
 
 def sort_array():
-    """
-    sorts array numerically
-    :return:
-    """
-    for i in range(0, len(numbers)):
-        for j in range(i + 1, len(numbers)):
-            if numbers[i] > numbers[j]:
-                temp = numbers[i]
-                numbers[i] = numbers[j]
-                numbers[j] = temp
-    return numbers  # return works with test statement
+    pass
 
 
 if __name__ == '__main__':
--- test_collections/test_sort_and_search_array.py
+++ test_collections/test_sort_and_search_array.py
@@ -11,7 +11,8 @@ class MyTestCase(unittest.TestCase):
     def test_itemNotFound(self):
         self.assertEqual(ss.search_array(), "-1")
 
-
+    def test_sortList(self):
+        self.assertEqual(ss.sort_array(), array('d', [11.5, 15.5, 20.5, 30.5]))
 
 
 if __name__ == '__main__':
